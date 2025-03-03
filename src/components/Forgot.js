ad60a42d4eda3ef8bb2f1c39b68ec1a7b30dfd4f
Barsharani <barsharani.padhy13@gmail.com>
2025-03-03 17:39:11 +0530

    Forgot Password page

--- src/components/Forgot.js
+++ src/components/Forgot.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "../styles/styles.css";
 
+
 export default function ForgotPass() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
